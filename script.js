ba57a89702f3786c08960da1a421ab4002d7dd69
Harshit Parvatiyar <71987068+Parvatiyar-Harshit@users.noreply.github.com>
2020-11-21 16:31:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function ageindays(){
 function reset(){
 	document.getElementById('res1').remove();
 }
+function generateCat(){
+	var image = document.createElement('img');
+	var div = document.getElementById('flex-cat-gen');
+	image.src = 'https://media.tenor.com/images/1a5a73dbaaaf7ec3f0da44fb75e58c76/tenor.gif'
+	div.appendChild(image);
+}
+
+function rpsGame(yourChoice){
+	var humanChoice , botChoice;
+	humanChoice = yourChoice.id;
+	botChoice = 
+}
